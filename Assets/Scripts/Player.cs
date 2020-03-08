855561f21da8fe68dc7c18f2dffdfc3a30d33617
victropic <victropic@gmail.com>
2020-03-08 01:00:31 -0600

    Run and Crouch added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,16 +10,29 @@ public class Player : MonoBehaviour
     float mouseX, mouseY;
     float jump;
 
-    /* Stats */
-    public float speed;
+    /* Movement Stats */
+    public float crouchSpeed;
+    public float walkSpeed;
+    public float runSpeed;
     public Vector2 lookSpeed;
     public float jumpIntensity;
     public float gravity;
 
+    /* Game Stats */
+    public float health;
+
     /* States */
+    public bool alive;
+    public bool vulnerable;
+    public bool running;
+    public bool crouching;
 
     /* Physics */
     float verticalSpeed;
+
+    /* Misc */
+    GameObject hand;
+    Transform upperBody;
 
     /* Components */
     Camera cam;
@@ -28,9 +41,13 @@ public class Player : MonoBehaviour
 
     void Start()
     {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
         cam = GetComponentInChildren<Camera>();
         rigid = GetComponent<Rigidbody>();
         charContr = GetComponent<CharacterController>();
+        upperBody = transform.GetChild(0);
     }
 
     void Update()
@@ -40,16 +57,30 @@ public class Player : MonoBehaviour
         mouseX = Input.GetAxis("Mouse X");
         mouseY = Input.GetAxis("Mouse Y");
         jump = Input.GetAxis("Jump");
+        running = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+        crouching = Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl);
 
         //player and camera rotation
         transform.rotation *=  Quaternion.AngleAxis(lookSpeed.x * Time.deltaTime * mouseX, Vector3.up);
         cam.transform.rotation *= Quaternion.AngleAxis(lookSpeed.y * Time.deltaTime * mouseY, Vector3.left);
-
-        //Movement
         
     }
 
     void FixedUpdate() {
+        float speed = walkSpeed;
+        if(running && !crouching) {
+            speed = runSpeed;
+        } else if(crouching) {
+            speed = crouchSpeed;
+            //upperBody.localPosition = new Vector3(0f, 0f, 0f);
+            charContr.height = 1f;
+        }
+
+        if(!crouching) {
+            //upperBody.localPosition = new Vector3(0f, 0.5f, 0f);
+            charContr.height = 2f;
+        }
+
         charContr.Move(transform.rotation * new Vector3(h, 0f, v).normalized * speed * Time.deltaTime + Vector3.up * verticalSpeed * Time.deltaTime);
 
         if(charContr.isGrounded) {

c456a9d5a7cc5bd7e523e29d72c735ad506916c5
victropic <victropic@gmail.com>
2020-03-07 21:36:43 -0600

    creating my own player script with basic fps controls for better control

---
+++ Assets/Flashlight/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    /* Input */
+    float h, v;
+    float mouseX, mouseY;
+
+    /* Stats */
+    public float speed;
+    public Vector2 lookSpeed;
+
+
+    /* Components */
+    Camera cam;
+
+    void Start()
+    {
+        cam = GetComponentInChildren<Camera>();
+    }
+
+    void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+
+        //player and camera rotation
+        transform.rotation *=  Quaternion.AngleAxis(lookSpeed.x * mouseX, Vector3.up);
+        cam.transform.rotation *= Quaternion.AngleAxis(lookSpeed.y * mouseY, Vector3.left);
+
+        //Movement
+        transform.position += transform.rotation * new Vector3(h, 0f, v).normalized * speed;
+    }
+}

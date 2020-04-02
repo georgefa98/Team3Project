67661a77a95aa98d85bc95a55d2cad8e31ef52ba
victropic <victropic@gmail.com>
2020-04-02 00:33:46 -0500

    Added hammer

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : Item
+{
+
+    protected bool aiming;
+
+    public virtual void StartAiming() {
+
+    }
+
+    public virtual void StopAiming() {
+
+    }
+
+}

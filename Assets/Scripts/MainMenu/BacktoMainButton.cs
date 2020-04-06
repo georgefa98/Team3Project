e78657c8c6f7c7f079cca33b49bcd3b6287a1bc3
victropic <victropic@gmail.com>
2020-04-05 21:45:22 -0500

    Main menu

---
+++ Assets/Scripts/MainMenu/BacktoMainButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BacktoMainButton : MonoBehaviour
+{
+
+    CanvasGroup controlsPanel;
+    public CanvasGroup mainPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controlsPanel = transform.parent.GetComponent<CanvasGroup>();
+    }
+
+    // Update is called once per frame
+    public void ShowMainMenu()
+    {
+        controlsPanel.alpha = 0;
+        controlsPanel.interactable = false;
+        controlsPanel.blocksRaycasts = false;
+        
+        mainPanel.alpha = 1;
+        mainPanel.interactable = true;
+        mainPanel.blocksRaycasts = true;
+    }
+}

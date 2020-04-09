d710de7a3b6172e4183bfa5818d50f10d442cd7e
dmart100 <martinez.d@outlook.com>
2020-04-08 23:22:49 -0500

    merge after cloning new master branch

--- Assets/Scripts/Environment/Projectiles.cs
+++ Assets/Scripts/Environment/Projectiles.cs
@@ -11,6 +11,7 @@ public class Projectiles : MonoBehaviour
     private Rigidbody projectile;
     private Transform player;
     private Vector3 target;
+    private float dropPoint = 2.5f, playerYPosition = 0.5f;
 
 
     public float speed;
@@ -46,7 +47,11 @@ public class Projectiles : MonoBehaviour
 
     void Path()
     {
+<<<<<<< HEAD
+        if ((Vector3.Distance(transform.position, player.position) > dropPoint) && (transform.position.y > playerYPosition))
+=======
         if ((Vector3.Distance(transform.position, player.position) > 3.5f) && (transform.position.y > 0.5f))
+>>>>>>> b2972463162f47b5e780857ff9c0950840071c04
         {
             transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
         }
